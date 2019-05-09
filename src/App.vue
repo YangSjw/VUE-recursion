1808a99e12fb609e3b20ff4ef5c27e1c8575a183
unknown <853613167@qq.com>
2019-05-09 16:05:36 +0800

    digui-component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HelloWorld :data="data.groups"></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import data from '../static/data'
+
+export default {
+  data(){
+    return {
+      data
+    }
+  },
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li v-for="(item,index) in data" :key="index">
+      {{item.name}}
+      <HelloWorld v-if="item.child" :data="item.child"></HelloWorld>
+      <HelloWorld v-if="item.groups" :data="item.groups"></HelloWorld>
+    </li>
+  </ul>
+</template>
+
+<script>
+import datas from '../../static/data'
+import HelloWorld from './HelloWorld'
+export default {
+  name:'HelloWorld',
+  data(){
+    return {
+      datas
+    }
+  },
+  components:{
+    HelloWorld
+  },
+  created() {
+
+  },
+  props:['data'],
+  mounted() {
+    // console.log(datas)
+  }
+}
+</script>
+<style scoped  lang="">
+
+</style>
